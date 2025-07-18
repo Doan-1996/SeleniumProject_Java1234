8f6262652c8af705d11c9fc5f11d42003a868dc9
doctdao <doctdao@gmail.com>
2025-07-18 14:51:53 +0700

    abc

--- Day5_4/TinhLuong.java
+++ Day5_4/TinhLuong.java
@@ -19,7 +19,7 @@ public class TinhLuong {
 	// Hàm số với 4 tham số truyền vào (lương cơ bản x hệ số lương x số năm kinh
 	// nghiệm + luongCoBan x loại)
 
-	//public static double TinhLuong(double hesoluong, int namKN, double loai) {
+	public static double TinhLuong(double hesoluong, int namKN, double loai) {
 		System.out.println("-> Tính lương theo công thức 4 tham số ");
 		return hesoluong * namKN * LUONG_CO_BAN + LUONG_CO_BAN * loai;
 	}
